169862118e66ad99d55f992c4e72e67b091075a0
MurdookCode1 <francofloreskevin03@gmail.com>
2022-08-30 18:15:15 -0500

    P01 Y TRABAJO DE IVNESTIGACION

---
+++ module_scala/src/main/scala/org/datascience/spark/Trabajo_De_Investigacion.scala
@@ -0,0 +1,28 @@
+package org.datascience.spark
+
+object Trabajo_De_Investigacion {
+
+  def main(args: Array[String]): Unit = {
+
+    // Este método se utiliza para devolver el código hash de una string.
+    // Scala program of hashCode() method
+    // Using hashCode() methods
+    val result = "Ayushi".hashCode()
+    println("Result : " + result)
+
+    //Este método se utiliza para la concatenación de las dos strings. Unió dos strings juntas y formó una sola string
+    // Concatenate two strings
+    // Using concat() methods
+    val result1 = "Geeks".concat("forGeeks")
+    println("Result : " + result1)
+
+
+    // Este metodo sirve para reemplazar dos caracteres
+    val result3 = "potdotnothotokayslot".replaceFirst(".ot", "**")
+    println("Result : " + result3)
+
+  }
+
+
+
+}
