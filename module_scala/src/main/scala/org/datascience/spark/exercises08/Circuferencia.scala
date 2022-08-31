84e46d6d864a6d2e95d9590670f0084e85ccd1d4
Kenyi Canchihuaman Ango <kenyica13@gmail.com>
2022-08-30 20:56:20 -0500

    base

--- module_scala/src/main/scala/org/datascience/spark/exercises08/Circuferencia.scala
+++
@@ -1,7 +0,0 @@
-package org.datascience.spark.exercises08
-
-case class Circuferencia(radio: Double) extends Figura {
-
-  override def calcularArea(): Double = radio * radio
-
-}
--- module_scala/src/main/scala/org/datascience/spark/exercises08/Rectangulo.scala
+++
@@ -1,7 +0,0 @@
-package org.datascience.spark.exercises08
-
-case class Rectangulo(largo: Int , ancho: Int) extends Figura {
-
-  override def calcularArea(): Double = largo * ancho
-
-}
