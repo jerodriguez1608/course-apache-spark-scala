ce484fa8d5242e3541eaf548d89f0932ad0b76b6
Kenyi Canchihuaman Ango <kenyica13@gmail.com>
2022-09-01 16:25:56 -0500

    resolucion del ejercicio07

--- module_scala/src/main/scala/org/datascience/spark/ejercicio07/pregunta_07.scala
+++ module_scala/src/main/scala/org/datascience/spark/ejercicio07/pregunta_07.scala
@@ -1,18 +1,25 @@
-package org.datascience.spark
+package org.datascience.spark.ejercicio07
 
 object pregunta_07 {
 
-  /***
+  /** *
    * Implementar una clase para que lea una fecha con el formato "dd/MM/yyyy" y
    * valide que sea una fecha correcta, también la fecha debe ser mayor al 01/01/1900,
    * la clase debe devolver el numero de días transcurridos desde la fecha ingresada
    * hasta la ultima copa del mundo.
+   *
    * @param args
    */
 
+  def resultadoValidarFecha(world: fecha): Unit ={
+    world.validarFecha()
+  }
+
   def main(args: Array[String]): Unit = {
 
-
+    //formato de fecha en Int
+    //dia,mes,anio
+    val resultadoFecha = resultadoValidarFecha(fecha(1,9,2022))
 
   }
 }
